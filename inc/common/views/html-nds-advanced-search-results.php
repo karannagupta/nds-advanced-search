75263a623d45f0288ced1d1e13ab3cd1ca6fe56c
Karan Gupta <nuancedesignstudio@users.noreply.github.com>
2017-12-22 00:29:15 +0530

    Fix for displaying the content with wp_trim_words

--- inc/common/views/html-nds-advanced-search-results.php
+++ inc/common/views/html-nds-advanced-search-results.php
@@ -30,30 +30,30 @@ $args = array(
 $search_query = new \WP_Query( $args );
 ?>
 <div class="nds-search-results">
-    <ul class="flex-grid-container">
-        <!-- Start the Loop. -->
-        <?php
-        if ( $search_query->have_posts() ) :
-            while ( $search_query->have_posts() ) :
-                $search_query->the_post();
-        ?>
+	<ul class="flex-grid-container">
+		<!-- Start the Loop. -->
+		<?php
+		if ( $search_query->have_posts() ) :
+			while ( $search_query->have_posts() ) :
+				$search_query->the_post();
+		?>
 
-                <li class="flex-grid-item">
+				<li class="flex-grid-item">
 
-                    <!-- the thumbnail -->
-                    <p>
-                        <?php if ( has_post_thumbnail() ) : ?>
-                            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'medium' ); ?></a>
-                        <?php endif; ?>
-                    </p>
-                    <!-- title -->
-                    <p class="card-title">
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    </p>
-                    <!-- excerpt -->
-                    <p class="card-excerpt">
-                        <?php esc_html_e( substr( get_the_content(), 0, 150 ) . ' ... ' ); ?>
-                    </p>
+					<!-- the thumbnail -->
+					<p>
+						<?php if ( has_post_thumbnail() ) : ?>
+							<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'medium' ); ?></a>
+						<?php endif; ?>
+					</p>
+					<!-- title -->
+					<p class="card-title">
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					</p>
+					<!-- excerpt -->
+					<p class="card-excerpt">
+						<?php echo wp_trim_words( get_the_content(), 30, ' ...' ); ?>
+					</p>
 
 				</li> <!-- flex-grid-item -->
 			<?php endwhile; ?>
@@ -62,7 +62,7 @@ $search_query = new \WP_Query( $args );
 		<?php echo __( 'Nothing Found ...', $this->plugin_text_domain ); ?>
 		<?php endif; ?>
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>></div>
-    </ul> <!-- flex-grid-container -->
+	</ul> <!-- flex-grid-container -->
 </div> <!-- nds-search-results -->
 
 
